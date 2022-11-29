e23caf471024416acaf202d15812a40bfbf9316a
gikimin <95573153+gikimin@users.noreply.github.com>
2022-11-30 06:09:06 +0900

    testst

--- Python/__default__.py
+++ Python/__default__.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
 Attendance_date = datetime(2022, 5, 12, 12, 30, 00, 0)            ######
-Mask_DB_Path = 'D:/Mask_Project/Senier-project/Python/user_img/Mask'
-NoMask_DB_Path ='D:/Mask_Project/Senier-project/Python/user_img/NoMask'
-Photo_Path = "D:/Mask_Project/Senier-project/Python/user_img/Temp"
-PKL_Mask_Path = "D:/Mask_Project/Senier-project/Python/user_img/Mask/representations_arcface.pkl"
-PKL_NoMask_Path = "D:/Mask_Project/Senier-project/Python/user_img/NoMask/representations_arcface.pkl"
+Mask_DB_Path = 'D:/Mask_Project/user_img/Mask'
+NoMask_DB_Path ='D:/Mask_Project/user_img/NoMask'
+Photo_Path = "D:/Mask_Project/user_img/Temp"
+PKL_Mask_Path = "D:/Mask_Project/user_img/Mask/representations_arcface.pkl"
+PKL_NoMask_Path = "D:/Mask_Project/user_img/NoMask/representations_arcface.pkl"
 pkl = "representations_arcface.pkl"
